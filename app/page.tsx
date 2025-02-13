7f9c19c4f3f96fbaf263ec143c9a6e9c5a63895a
Christopher Nathanael Wijaya <christophernathanael10@gmail.com>
2025-02-13 17:17:41 +0700

    test again

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Hello World! 🚀</h1>
       <p>This is running locally!</p>
-      <p>TESTING ===============================</p>
+      <p>PPL B - Kelompoka 6</p>
     </div>
   );
 }
