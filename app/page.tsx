c068d7a0504aeae234ccd3a65735e2fdab16cf99
Christopher Nathanael Wijaya <christophernathanael10@gmail.com>
2025-02-13 10:32:21 +0700

    hello world

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello World! 🚀</h1>
+      <p>This is running locally!</p>
+    </div>
+  );
+}
