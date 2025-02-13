754783a1395705f391843d299357db56a4f6aef6
Christopher Nathanael Wijaya <christophernathanael10@gmail.com>
2025-02-13 17:38:40 +0700

    fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Hello World! 🚀</h1>
       <p>This is running locally!</p>
-      <p>PPL B - Kelompoka 6</p>
+      <p>PPL B - Kelompok 6</p>
     </div>
   );
 }
