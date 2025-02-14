30dc713f810f37709215e4a4a94567bb485e9def
Christopher Nathanael Wijaya <christophernathanael10@gmail.com>
2025-02-14 19:01:20 +0700

    coba sonar fe

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
+      {/* asdfasdfasdf */}
       <h1>Hello World! 🚀</h1>
       <p>This is running locally!</p>
       <p>PPL B - Kelompok 6</p>
