17f91e75ffa5d8d4620010f9945678c78618216f
HilmyAmmar <hilmyammardarmawan17@gmail.com>
2025-03-02 01:48:24 +0700

    [RED] add test for multirole account setting

---
+++ __tests__/multirole/MultiRole.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import MultiRolePage from "@/src/app/multirole/page"; 
+
+
+describe("User Accounts Page", () => {
+  it("should display the title 'User Accounts'", () => {
+    render(<MultiRolePage/>);
+    expect(screen.getByText("Pengaturan Pengguna")).toBeInTheDocument();
+  });
+
+  it("should display the list of users with roles and email", () => {
+    render(<MultiRolePage/>);
+
+    expect(screen.getByText("Hilmy Ammar Darmawan")).toBeInTheDocument();
+    expect(screen.getByText("hilmyammardarmawan@gmail.com")).toBeInTheDocument();
+    expect(screen.getByText("Pemilik")).toBeInTheDocument();
+    
+    expect(screen.getByText("Fadrian Yhoga Pratama")).toBeInTheDocument();
+    expect(screen.getByText("fadrianyhogapratama@gmail.com")).toBeInTheDocument();
+    expect(screen.getByText("Kasir 1")).toBeInTheDocument();
+    
+    expect(screen.getByText("Nadhira Raihana Hafeez")).toBeInTheDocument();
+    expect(screen.getByText("nadhiraraihanahafeez@gmail.com")).toBeInTheDocument();
+    expect(screen.getByText("Kasir 2")).toBeInTheDocument();
+
+  });
+
+  it("should display a button to add new accounts", () => {
+    render(<MultiRolePage/>);
+    expect(screen.getByText("+ Tambah Akun")).toBeInTheDocument();
+  });
+});
