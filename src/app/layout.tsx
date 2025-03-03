c1185643a91a85de4d9b1c7067343abdf49c6964
nadriha <hafez.nadhira@gmail.com>
2025-03-04 00:14:52 +0700

    [REFACTOR] fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} min-h-screen overflow-x-hidden custom-scrollbar-hidden antialiased flex justify-center`}
       >
-        <div className="w-full sm:w-[402px] min-h-screen bg-[#EDF1F9] p-3">
-            {children}
+        <div className="w-full sm:w-[402px] min-h-screen bg-[#EDF1F9]">
+            <Navbar />
+            <div className="p-3 h-full pb-32">
+              {children}
+            </div>
         </div>
       </body>
     </html>
