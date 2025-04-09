c8980bc63e0265c3d53840939e594f7e6a96ff9c
HilmyAmmar <hilmyammardarmawan17@gmail.com>
2025-04-09 23:49:02 +0700

    [REFACTOR] applying separation of concern for adduser page

--- src/app/(withNavbar)/multirole/adduser/page.tsx
+++ src/app/(withNavbar)/multirole/adduser/page.tsx
@@ -10,6 +10,8 @@ import { sendInvitation } from "../adduser/services/invitationService";
 import { InvitationPayload, InvitationResponse } from "./types/types";
 import { Modal } from '@/src/components/elements/modal/Modal'
 import config from "@/src/config";
+import { ConfirmModal } from "./components/confirmmodal";
+
 
 export default function AddUserPage() {
   const router = useRouter();
@@ -163,52 +165,16 @@ export default function AddUserPage() {
         </button>
       </form>
       {showConfirmModal && (
-      <Modal onClose={() => setShowConfirmModal(false)}>
-          <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            submitConfirmed(e);
-          }}
-          className="w-full max-w-md"
-          >
-          <div role="dialog">
-            <h2 className="text-base font-semibold text-center">Ringkasan Pengguna Baru</h2>
-
-            <div className="space-y-3 w-full text-left text-sm">
-              <div>
-                <p className="text-gray-500">Nama</p>
-                <p className="font-semibold break-words">{name}</p>
-              </div>
-              <div>
-                <p className="text-gray-500">Role</p>
-                <p className="font-semibold">{role}</p>
-              </div>
-              <div>
-                <p className="text-gray-500">Email</p>
-                <p className="font-semibold break-words">{email}</p>
-              </div>
-            </div>
-
-            <div className="flex justify-between gap-4 w-full mt-6">
-              <button
-                type="button"
-                onClick={() => setShowConfirmModal(false)}
-                className="flex-1 px-4 py-2 rounded-xl bg-gray-300 hover:bg-gray-400"
-              >
-                Batal
-              </button>
-              <button
-                type="submit"
-                className="flex-1 px-4 py-2 rounded-xl bg-blue-600 text-white hover:bg-blue-700"
-              >
-                Ya, kirim
-              </button>
-            </div>
-          </div>
-        </form>
-      </Modal>
-    )}
-
+        <Modal onClose={() => setShowConfirmModal(false)}>
+          <ConfirmModal
+            name={name}
+            role={role}
+            email={email}
+            onClose={() => setShowConfirmModal(false)}
+            onConfirm={submitConfirmed}
+          />
+        </Modal>
+      )}
     </div>
   );
 }
