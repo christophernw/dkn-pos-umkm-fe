b50a52753a3450f13a257b2d7d36d233c990c41e
christophernw <christophernathanael10@gmail.com>
2025-04-09 13:08:32 +0700

    [REFINE] Update button styling and functionality in SemuaBarang page to match Transaksi page

--- src/app/(withNavbar)/semuaBarang/page.tsx
+++ src/app/(withNavbar)/semuaBarang/page.tsx
@@ -4,8 +4,12 @@ import HeaderProduk from "@/src/components/HeaderProduk";
 import ProductCard from "@/src/components/ProductCard";
 import React, { Suspense } from "react";
 import { useAuth } from "@/contexts/AuthContext";
+import { useRouter } from "next/navigation";
+import { PlusIcon } from "@/public/icons/PlusIcon";
+
 const SemuaBarang = () => {
   const { user } = useAuth();
+  const router = useRouter();
 
   return (
     <div className="relative min-h-screen">
@@ -21,16 +25,14 @@ const SemuaBarang = () => {
       <Suspense fallback={<div>Loading...</div>}>
         <ProductCard />
       </Suspense>
-      <div className="fixed bottom-4 flex justify-end px-4 pb-24">
-        <button
-          className="bg-blue-600 hover:bg-blue-700 text-white text-4xl flex items-center justify-center h-14 w-14 font-medium rounded-full shadow-md"
-          onClick={() => (window.location.href = "/tambahProduk")}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 256 256">
-            <path fill="currentColor" d="M228 128a12 12 0 0 1-12 12h-76v76a12 12 0 0 1-24 0v-76H40a12 12 0 0 1 0-24h76V40a12 12 0 0 1 24 0v76h76a12 12 0 0 1 12 12" />
-          </svg>
-        </button>
-      </div>
+      
+      {/* Updated button to match Transaksi page styling */}
+      <button
+        className="bg-primary-indigo rounded-full w-fit fixed bottom-4 right-4 sm:right-[calc(50%-(420px/2)+1rem)] p-4 mb-24"
+        onClick={() => router.push("/tambahProduk")}
+      >
+        <PlusIcon />
+      </button>
     </div>
   );
 };
