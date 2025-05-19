01cc019a38217727de9df1e22314639d0150696e
Christopher Nathanael Wijaya <christophernathanael10@gmail.com>
2025-05-19 11:48:31 +0700

    [ADD] Extend user roles to include BPR and implement routing restrictions for BPR users

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,15 +1,32 @@
-import NextAuth, { User } from "next-auth";
+import NextAuth, { DefaultSession, DefaultUser, JWT } from "next-auth";
+import { DefaultJWT } from "next-auth/jwt";
 
 declare module "next-auth" {
-    interface User extends DefaultUser {
-        id: string;
-        email: string;
-        name: string;
-        users: User[];
-        picture: string;
-    }
+  interface User extends DefaultUser {
+    id: string;
+    email: string;
+    name: string;
+    users: User[];
+    picture: string;
+    is_bpr?: boolean;
+    role?: string;
+  }
 
-    interface Session {
-        user: User;
+  interface Session extends DefaultSession {
+    user: User;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    user?: {
+      id: string;
+      email: string;
+      name: string;
+      users?: any[];
+      picture?: string;
+      is_bpr?: boolean;
+      role?: string;
     }
+  }
 }
