dd4a2815342a601b9e4a57079db8b3e75ba4e372
Yhoga <yhogafadrian@gmail.com>
2025-05-20 13:10:16 +0700

    sonarqube

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,27 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/display-name": "off", // Disable the ESLint rule
+      "@typescript-eslint/no-explicit-any": "off", // Disable the ESLint rule
+      "@typescript-eslint/no-unused-vars": "off", // Disable the ESLint rule
+      "@next/next/no-img-element": "off", // Disable the ESLint rule
+      "react-hooks/exhaustive-deps": "off",
+      "prefer-const": "off",
+      "react-hooks/rules-of-hooks": "off" // Add this line to disable the hooks rule
+    },
+  },
+];    
+
+export default eslintConfig;
