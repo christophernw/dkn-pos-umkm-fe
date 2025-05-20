738182534388e9f692fe76e9f7f0fbd7448b8064
Yhoga <yhogafadrian@gmail.com>
2025-05-20 09:27:17 +0700

    sonar

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,13 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "react/display-name": "off", // Disable the ESLint rule
-      "@typescript-eslint/no-explicit-any": "off", // Disable the ESLint rule
-      "@typescript-eslint/no-unused-vars": "off", // Disable the ESLint rule
-      "@next/next/no-img-element": "off", // Disable the ESLint rule
-      "react-hooks/exhaustive-deps": "off",
-    },
-  },
-];    
-
-export default eslintConfig;
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
