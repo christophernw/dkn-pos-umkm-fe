e6bf7bbf4165203034f5724def19fe3f1abc5720
Christopher Nathanael Wijaya <christophernathanael10@gmail.com>
2025-05-22 06:10:00 +0700

    [REFactor] Comment out unused transaction category options

--- src/app/tambahTransaksi/const/transactionCategoryOptions.ts
+++ src/app/tambahTransaksi/const/transactionCategoryOptions.ts
@@ -6,14 +6,14 @@ export const pemasukanOptions = [
             { label: "Pendapatan Jasa/Komisi", value: "Pendapatan Jasa/Komisi" },
             { label: "Terima Pinjaman", value: "Terima Pinjaman" },
             { label: "Penagihan Utang/Cicilan", value: "Penagihan Utang/Cicilan" },
-            { label: "Pembelian Stok", value: "Pembelian Stok" },
-            { label: "Pembelian Bahan Baku", value: "Pembelian Bahan Baku" },
-            { label: "Biaya Operasional", value: "Biaya Operasional" },
-            { label: "Gaji/Bonus Karyawan", value: "Gaji/Bonus Karyawan" },
-            { label: "Pemberian utang", value: "Pemberian utang" },
-            { label: "Pembayaran Utang/Cicilan", value: "Pembayaran Utang/Cicilan" },
-            { label: "Pengeluaran Di Luar Usaha", value: "Pengeluaran Di Luar Usaha" },
-            { label: "Pengeluaran lain-lain", value: "Pengeluaran lain-lain" },
+            // { label: "Pembelian Stok", value: "Pembelian Stok" },
+            // { label: "Pembelian Bahan Baku", value: "Pembelian Bahan Baku" },
+            // { label: "Biaya Operasional", value: "Biaya Operasional" },
+            // { label: "Gaji/Bonus Karyawan", value: "Gaji/Bonus Karyawan" },
+            // { label: "Pemberian utang", value: "Pemberian utang" },
+            // { label: "Pembayaran Utang/Cicilan", value: "Pembayaran Utang/Cicilan" },
+            // { label: "Pengeluaran Di Luar Usaha", value: "Pengeluaran Di Luar Usaha" },
+            // { label: "Pengeluaran lain-lain", value: "Pengeluaran lain-lain" },
         ]
 
 export const pengeluaranOptions = [
